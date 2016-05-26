bafd7a3dc8e39817cd847f22c8a3623a976c752a
DEEPAK KUMAR TIWARI <kumar.deepaktiwari@gmail.com>
2016-05-26 16:29:54 +0530

    Formatting

--- src/Utils/Getprocedurestat.java
+++ src/Utils/Getprocedurestat.java
@@ -2,14 +2,16 @@ package Utils;
 
 import java.io.IOException;
 import java.sql.SQLException;
-
 import org.apache.log4j.Logger;
 import org.testng.asserts.SoftAssert;
-
 import Connection_DB.Connection_Parameter;
-
+import Filereader.Propertyfilereader;
 public class Getprocedurestat extends Connection_Parameter {
 	
+	public int lastelapsedtimetakenbyprocedure;
+	public static int SPReturnParam;
+	public static boolean IsResultSetDataAvailable;
+	public String Param;
 	/* This class is responsible for getting the performance statistics for supplied stored procedure
 	 * 
 	 */
@@ -32,6 +34,20 @@ public class Getprocedurestat extends Connection_Parameter {
 		try {
 			App_log.info("Fetching Statistics.....");
 
+		/*	 TOP 1 is added because performance statistics shows multiple rows as a result in performance statistics
+			 App_log.info("Printing query " + ("SELECT  TOP 1 d.object_id" +
+			  "," + " d.database_id" + "," + "OBJECT_NAME" + "(" + "object_id"
+			  + "," + "database_id" + ")" + " '" + "proc name" + "'" + "," +
+			  "d.cached_time" + "," + "d.last_execution_time" + "," +
+			  " d.total_elapsed_time" + "," + "(" + "d.total_elapsed_time" +
+			  "/" + "d.execution_count" + ")" + "/" + "1000 AS " + "[" +
+			  "avg_elapsed_time" + "]" + "," + "d.last_elapsed_time" + "/" +
+			  "1000 as last_elapsed_time" + "," + "d.execution_count" + "," +
+			  "*" + "FROM sys.dm_exec_procedure_stats AS d" +
+			  " Where OBJECT_NAME" + "(" + "object_id" + "," + "database_id" +
+			  ")" + "=" +"'"+ssp_name+"'"+" Order by d.Last_Execution_Time DESC ")); */
+			 
+
 			stmt = conn.createStatement();
 			rs = stmt.executeQuery("SELECT TOP 1 d.object_id" + ","
 					+ " d.database_id" + "," + "OBJECT_NAME" + "("
@@ -46,12 +62,13 @@ public class Getprocedurestat extends Connection_Parameter {
 					+ "FROM sys.dm_exec_procedure_stats AS d"
 					+ " Where OBJECT_NAME" + "(" + "object_id" + ","
 					+ "database_id" + ")" + "=" +"'"+ssp_name+"'"+" Order by d.Last_Execution_Time DESC ");
-
+			
 			while (rs.next()) {
 				String Averageelapsedtime = rs.getString("avg_elapsed_time");
 				String Lastelapsedtime = rs.getString("last_elapsed_time");
 				String LastExecutionTime = rs.getString("last_execution_time");
 				String ExecutionCount = rs.getString("execution_count");
+				String TotalWorkerTime = rs.getString("total_worker_time");
 
 				App_log.info("******************************************");
 
@@ -59,7 +76,8 @@ public class Getprocedurestat extends Connection_Parameter {
 						+ Averageelapsedtime + "--Lastelapsedtime --"
 						+ Lastelapsedtime + "--LastExecutionTime--"
 						+ LastExecutionTime + "--ExecutionCount --"
-						+ ExecutionCount);
+						+ ExecutionCount + "--TotalWorkerTime--"
+						+ TotalWorkerTime);
                 /*
                  *  Object of Filewrite class has been created to write data to files 
                  *  File will be created with names of stored procedure
@@ -68,34 +86,36 @@ public class Getprocedurestat extends Connection_Parameter {
                  */
 				Filewrite Forwarddata = new Filewrite();
 				Forwarddata.Writestatisticstofile(ssp_name, Averageelapsedtime,
-						Lastelapsedtime, LastExecutionTime, ExecutionCount);
+						Lastelapsedtime, LastExecutionTime, ExecutionCount,TotalWorkerTime);
+				
+				if(IsResultSetDataAvailable){
+					Param="YES";
+				}else{
+					Param="NO";
+				}
 
 				Forwarddata.Writeonlytimeandcounttofile(ssp_name,
-						Lastelapsedtime, ExecutionCount);
+						Lastelapsedtime, ExecutionCount ,SPReturnParam,Param,TotalWorkerTime);
 
-				int lastelapsedtimetakenbyprocedure = Integer
+				lastelapsedtimetakenbyprocedure = Integer
 						.parseInt(Lastelapsedtime);
 				App_log.info("During test checking Lastelapsedtime "
 						+ lastelapsedtimetakenbyprocedure);
-				
-				ssp_name=null;
-				
-				//Thread.sleep(2000l);
-				if ( lastelapsedtimetakenbyprocedure>10) {
-					Reportfailure.assertTrue(false,
-							"Last elapsed time is more than 10");
-					Reportfailure.assertAll();
 				}
-			}
 		} catch (Throwable t) {
 			App_log.info("Error in fetching performance statistics for "
-					+ ssp_name);
+					+ ssp_name+" Error is "+t.fillInStackTrace());
 			/*
 			 * Adding errors to basket
 			 * Errors will appear in report if test is run using testng.xml
 			 */
 			ErrorUtil.addVerificationFailure(t);
 		}
+		Propertyfilereader pfr = new Propertyfilereader();
+		pfr.Loadpropertyfile();
+		int sp_LETcheck=Integer.parseInt(Propertyfilereader.loadfile.getProperty("lastelapsedtimecheck"));
+		Reportfailure.assertTrue(lastelapsedtimetakenbyprocedure < sp_LETcheck,"Last elapsed time expected less than " +sp_LETcheck+" !!!");
+		Reportfailure.assertAll();
 	}
 
 }
